47436a83c6748508c2ae960e399fa3307de7c73a
salahsamy <salahsamy512@gmail.com>
2021-09-07 01:38:48 +0200

    added toolbar to activity

---
+++ app/src/main/java/com/example/sampleinsuranceapp/app/features/services/ServicesAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.sampleinsuranceapp.app.features.services
+
+class ServicesAdapter {
+}
---
+++ app/src/main/java/com/example/sampleinsuranceapp/app/features/services/ServicesFragment.kt
@@ -0,0 +1,4 @@
+package com.example.sampleinsuranceapp.app.features.Services
+
+class ServicesFragment {
+}
